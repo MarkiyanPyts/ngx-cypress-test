7ded909cf2c38b5769c286d064f1e486ec30169b
Markiyan Pyts <markiyan.pyts@osf.digital>
2024-06-21 14:32:27 +0300

    work

---
+++ cypress/e2e/first-test.spec.ts
@@ -0,0 +1,58 @@
+/// <reference types="cypress" />
+
+describe('First Test Suite', () => {
+    it('first test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // by tag name
+        cy.get('input')
+
+        // by tag id
+        cy.get('#inputEmail1')
+
+        // by tag class
+        cy.get('.input-full-width')
+
+        // by tag attribute name
+        cy.get('[fullwidth]')
+
+        // by tag attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        // by entire class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by 2 attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        // by tag, attribute id and class
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        // by cypress test id
+        cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it('second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // Theory
+        // get() - find elements on the page by locator globally...even when chained after other cypress methods
+        // find() - find child elements by locator, can only be chained after other cypress methods
+        // contains() - find elements by HTML text and by text and locator
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+        cy.contains('nb-card', 'Horizontal form').find('button')
+        cy.contains('nb-card', 'Horizontal form').contains('Sign in')
+        cy.contains('nb-card', 'Horizontal form').get('button')
+
+        // cypress chains and DOM
+        cy.get('#inputEmail3').parents('form').find('button').should('contain', 'Sign in')
+            .parents('form').find('nb-checkbox').click()
+        
+    })
+})
