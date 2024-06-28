699861c46f35a7076e27b50dcabf85cf53188714
Markiyan Pyts <markiyan.pyts@osf.digital>
2024-06-28 21:41:04 +0300

    Update first-test.spec.ts

--- cypress/e2e/first-test.spec.ts
+++ cypress/e2e/first-test.spec.ts
@@ -175,7 +175,7 @@ describe('First Test Suite', () => {
         })
     })
 
-    it.only('Lists and dropdowns', () => {
+    it('Lists and dropdowns', () => {
         cy.visit('/')
 
         // 1
@@ -203,6 +203,33 @@ describe('First Test Suite', () => {
                     cy.wrap(dropdown).click()
                 }
             })
+        })
+    })
+
+    it.only('Web tables', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        // 1 Get the row by text
+        cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        // 2 Get the row by index
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then(tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Mark')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Smith')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+
+        cy.get('tbody tr').first().find('td').then(tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Mark')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Smith')
         })
     })
 })
