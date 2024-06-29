d9d9cfafb36c14ad596f376f842d44585073de62
Markiyan Pyts <markiyan.pyts@osf.digital>
2024-06-29 21:27:51 +0300

    work

--- cypress/e2e/test-with-page-object.ts
+++ cypress/e2e/test-with-page-object.ts
@@ -1,4 +1,8 @@
 import {navigateTo} from '../support/page_objects/navigationPage'
+import {onFormLayoutsPage} from '../support/page_objects/formLayoutsPage'
+import { on } from 'cluster';
+import { onDatePickerPage } from '../support/page_objects/datePickerPage';
+import { onSmartTablePage } from '../support/page_objects/smartTablePage';
 
 describe('Test with Page Object', () => {
     beforeEach(() => {
@@ -12,4 +16,19 @@ describe('Test with Page Object', () => {
         navigateTo.tooltipPage();
         navigateTo.toasterPage();
     });
+
+    it('should submit Inline and Basic form and select tomorrow date in calendar', () => {
+        navigateTo.formLayoutsPage();
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('John Doe', 'test@test.com');
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'password');
+
+        navigateTo.datepickerPage();
+        onDatePickerPage.selectCommonDatepickerDateFromToday(1);
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14);
+
+        navigateTo.smartTablePage();
+        onSmartTablePage.addNewRecordWithFirstAndLastName('John', 'Doe');
+        onSmartTablePage.updateAgeByFirstName('John', 30);
+        onSmartTablePage.deleteRowByIndex(1);
+    })
 });
